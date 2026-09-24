191cfac9871e80da78b556667715b6e31beca68b
PranshulBajaj <pranshulmsit@gmail.com>
2026-09-24 15:32:20 +0530

    fix

--- ai-service/main.py
+++ ai-service/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://roadvision-ai-omega.vercel.app",
+        "http://localhost:5173"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
